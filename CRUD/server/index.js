ecef97d576ae4b46c64c7935433a2c50b9d434fa
VinicinBraga <vinicius@bragamatta.com.br>
2021-10-20 16:13:52 -0300

    Criando estrutura Inicial - Back-end

---
+++ CRUD/server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/", (req, res) => res.send());
+app.delete("/", (req, res) => res.send());
+app.put("/", (req, res) => res.send());
+
+app.listen(port, () => console.log(`Example app listening on port port!`));
